a6bacde31f55270b92d0fb98c1a0e6a792b57b3e
Truong Duy Linh <linhtruong6404@gmail.com>
2025-01-02 15:29:55 +0700

    Revert "update-css-plugin"
    This reverts commit 3653c5adae352bf464e79217ff811838af247f3e.

--- includes/class-init.php
+++ includes/class-init.php
@@ -9,6 +9,7 @@ class Class_Init
   {
     add_action('admin_menu', [$this, 'vnx_register_menu']);
     add_action('wp_enqueue_scripts', [self::class, 'enqueue_frontend_assets']);
+    add_action('admin_enqueue_scripts', [self::class, 'enqueue_admin_assets']);
   }
   public function vnx_register_menu()
   {
@@ -24,17 +25,18 @@ class Class_Init
   }
   public function vietnix_element_options_page()
   {
-    echo '<div class="w-full h-fit bg-red-300">';
     echo '<h1>Vietnix Element Options</h1>';
     echo '<p>Hello</p>';
   }
   public static function enqueue_frontend_assets()
   {
-    wp_enqueue_style('vnx-element-admin-css', VNX_PLUGIN_URL. 'assets/css/admin.css', [], VNX_PLUGIN_VERSION);
-    wp_enqueue_style('vnx-element-frontend-css', VNX_PLUGIN_URL . 'assets/css/frontend.css', [], VNX_PLUGIN_VERSION);
-
-    // Enqueue JS nếu cần
-    wp_enqueue_script('vnx-element-frontend-js', VNX_PLUGIN_URL . 'assets/js/frontend.js', ['jquery'], VNX_PLUGIN_VERSION, true);
+    wp_enqueue_style('my-plugin-frontend', VNX_PLUGIN_URL . 'assets/css/frontend.css', [], VNX_PLUGIN_VERSION);
+    wp_enqueue_script('my-plugin-frontend', VNX_PLUGIN_URL . 'assets/js/frontend.js', ['jquery'], VNX_PLUGIN_VERSION, true);
   }
-  
+
+  public static function enqueue_admin_assets()
+  {
+    wp_enqueue_style('my-plugin-admin', VNX_PLUGIN_URL . 'assets/css/admin.css', [], VNX_PLUGIN_VERSION);
+    wp_enqueue_script('my-plugin-admin', VNX_PLUGIN_URL . 'assets/js/admin.js', ['jquery'], VNX_PLUGIN_VERSION, true);
+  }
 }
